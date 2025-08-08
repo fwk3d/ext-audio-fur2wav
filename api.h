063d7298acac69b52174bce6f2709f829309f090
r-lyeh <35402248+r-lyeh@users.noreply.github.com>
2025-08-08 10:46:07 +0200

    Update api.h
    Remove unused v2 call

--- api.h
+++ api.h
@@ -3,7 +3,6 @@
 // note: furnace can also import mod/s3m/xm/it
 
 AUTORUN {
-    if( is_file("ext/ext-audio-fur2wav/furnace" ifdef(win32,".exe", ifdef(linux,".linux",".osx"))) )
     recipe(
         // paths and extensions. expected errorlevel
         "**.fur", 0,
@@ -13,3 +12,4 @@ AUTORUN {
 }
 
 #endif
+
